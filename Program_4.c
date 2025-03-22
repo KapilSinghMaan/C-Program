1bdcbae4cc6e373f07ca620e6c375e6737bfd565
KapilSinghMaan <kapilmaan369@gmail.com>
2025-03-22 20:02:20 +0530

    Sum of two numbers

---
+++ Program_4.c
@@ -0,0 +1,15 @@
+// Program to print Sum of two numbers entered by user 
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b;
+    printf("Enter First Number:");
+    scanf("%d",&a);
+    printf("\nEnter Second Number:");
+    scanf("%d",&b);
+    int sum=a+b;
+    printf("The result on adding %d and %d is : %d",a,b,sum);
+    getch();
+}
