06594316b5c14f8e1bdc16faf879a71e62c183a4
KapilSinghMaan <kapilmaan369@gmail.com>
2025-03-24 10:15:19 +0530

    Swap two numbers using third variable

---
+++ Program_8.c
@@ -0,0 +1,18 @@
+// Program to swap two numbers using third variable.
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,temp;
+    printf("Enter the value of a : ");
+    scanf("%d",&a);
+    printf("\nEnter the value of b : ");
+    scanf("%d",&b);
+    temp=a;
+    a=b;
+    b=a;
+    printf("\nAfter swapping value of a = %d",a);
+    printf("\nAfter swapping value of b = %d",b);
+    getch();
+}
