1d5582613b969f96456b9a77fbdf847050921015
KapilSinghMaan <kapilmaan369@gmail.com>
2025-03-26 10:31:52 +0530

    Area and Perimeter of Rectangle

---
+++ Program_15.c
@@ -0,0 +1,17 @@
+// Program to find area and perimeter of rectangle.
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,area,peri;
+    printf("Enter Length of Rectangle : ");
+    scanf("%d",&a);
+    printf("\nEnter Breath of Rectangle : ");
+    scanf("%d",&b);
+    area=(a*b);
+    peri=2*(a+b);
+    printf("\nArea of Rectangle is %d",area);
+    printf("\nPerimeter of Rectangle is %d",peri);
+    getch();
+}
