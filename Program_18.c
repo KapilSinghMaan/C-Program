1855f7d0d1829ac720d509dd9a8a8c6826677748
KapilSinghMaan <kapilmaan369@gmail.com>
2025-03-26 10:35:38 +0530

    Check alphabet - Vowel or Consonant

---
+++ Program_18.c
@@ -0,0 +1,19 @@
+// Program to check whether the entered alphabet is vowel or consonant.
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char ch;
+    printf("Enter any lowercase alphabet : ");
+    scanf("%c",&ch);
+    if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+    {
+        printf("\n%c is a Vowel",ch);
+    }
+    else
+    {
+        printf("\n%c is a Consonant",ch);
+    }
+    getch();
+}
