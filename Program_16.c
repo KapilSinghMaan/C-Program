f1c0febc47315de209b302428bf206bdf5f8354c
KapilSinghMaan <kapilmaan369@gmail.com>
2025-03-26 10:33:37 +0530

    Check number - Positive, Negative or Zero

---
+++ Program_16.c
@@ -0,0 +1,23 @@
+// Program to find whether entered number is Positive , Negative or Zero.
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num;
+    printf("Enter any Integer : ");
+    scanf("%d",&num);
+    if(num>0)
+    {
+        printf("\n%d is Positive Number",num);
+    }
+    else if(num==0)
+    {
+        printf("\n%d is Zero",num);
+    }
+    else
+    {
+        printf("\n%d is a Negative Number",num);
+    }
+    getch();
+}
