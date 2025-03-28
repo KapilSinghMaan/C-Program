f28286c78e9ded45c78fb92dab237c866a05717a
KapilSinghMaan <kapilmaan369@gmail.com>
2025-03-28 20:27:03 +0530

    Printing all Alphabets

---
+++ Program_21.c
@@ -0,0 +1,15 @@
+// Program to print all alphabet.
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    printf("Alphabets are :\n");
+    int i;
+    char ch;
+    for (i = 65; i <=90; i++)
+    {
+        ch=(char)i;
+        printf("%c\n",ch);
+    }
+}
