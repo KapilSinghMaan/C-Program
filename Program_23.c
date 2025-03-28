27a67a5586c01ce3222b7743ad17fd026065ea33
KapilSinghMaan <kapilmaan369@gmail.com>
2025-03-28 20:29:34 +0530

    Find factorial of a number

---
+++ Program_23.c
@@ -0,0 +1,16 @@
+// Program to find factorial of a number.
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,i,f=1;
+    printf("Enter any number : ");
+    scanf("%d",&n);
+    for ( i = 1; i <=n; i++)
+    {
+        f=f*i;
+    }
+    printf("\nFactorial of %d is %d",n,f);
+    getch();
+}
