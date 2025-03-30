dfe3db44f1faef453a22b3ab996eccd50686c88e
KapilSinghMaan <kapilmaan369@gmail.com>
2025-03-30 19:19:47 +0530

    Check Armstrong Number

---
+++ Program_28.c
@@ -0,0 +1,26 @@
+// Program to check whether entered number is Armstrong Number or not.
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num,temp,sum;
+    printf("Enter any Number : ");
+    scanf("%d",&num);
+    temp=num;
+    while (num!=0)
+    {
+        int a=num%10;
+        sum=sum+(a*a*a);
+        num=num/10;
+    }
+    if (sum==temp)
+    {
+        printf("\n%d is an Armstrong Number",temp);
+    }
+    else
+    {
+        printf("\n%d is not an Armstrong Number",temp);
+    }
+    getch();
+}
