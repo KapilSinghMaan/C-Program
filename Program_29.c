9215cb44ef3dd3a4d733eea7531e52675bb0b05a
KapilSinghMaan <kapilmaan369@gmail.com>
2025-03-30 19:20:39 +0530

    Printing Armstrong Number till 1000

---
+++ Program_29.c
@@ -0,0 +1,26 @@
+// Program to print all Armstrong number till 1000.
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int i,num,temp,sum=0,a;
+    for(i=1;i<=1000;i++)
+    {
+        num=i;
+        temp=i;
+        while (num!=0)
+        {
+            a=num%10;
+            sum=sum+(a*a*a);
+            num=num/10;
+        }
+        if (sum==temp)
+        {
+            printf("\n%d is an Armstrong Number",temp);
+        }
+        sum=0;
+        temp=0;
+    }
+    getch();
+}
