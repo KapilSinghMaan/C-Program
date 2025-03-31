ec29cc2f7db100fb3b9cc020f23faaa5e07acb96
KapilSinghMaan <kapilmaan369@gmail.com>
2025-03-31 20:29:06 +0530

    Check a Pallindrome number

---
+++ Program_32.c
@@ -0,0 +1,26 @@
+// Program to check whether entered number is Pallindrome number or not.
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int num,temp,rev;
+    printf("Enter any Number : ");
+    scanf("%d",&num);
+    temp=num;
+    while (num!=0)
+    {
+        int a=num%10;
+        rev=a+(10*rev);
+        num=num/10;
+    }
+    if (rev==temp)
+    {
+        printf("\n%d is a Palindrome Number",temp);
+    }
+    else
+    {
+        printf("\n%d is not a Palindrome Number",temp);
+    }
+    getch();
+}
