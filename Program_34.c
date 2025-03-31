bafda1599e28254c33725bf90950843776a14229
KapilSinghMaan <kapilmaan369@gmail.com>
2025-03-31 20:30:25 +0530

    Check Neon Number

---
+++ Program_34.c
@@ -0,0 +1,26 @@
+// Program to check whether a number is Neon Number or not.
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int n,sq,sum=0;
+    printf("Enter any Number : ");
+    scanf("%d",&n);
+    sq=n*n;
+    while (sq!=0)
+    {
+        int a=sq%10;
+        sum=sum+a;
+        sq=sq/10;
+    }
+    if (sum==n)
+    {
+        printf("\n%d is a Neon Number",n);
+    }
+    else
+    {
+        printf("\n%d is not a Neon Number",n);
+    }
+    getch();
+}
