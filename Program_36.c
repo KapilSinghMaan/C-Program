7961784fec0c92f1bc34acbbd3e5d6da5b0e9e8b
KapilSinghMaan <kapilmaan369@gmail.com>
2025-04-01 22:13:17 +0530

    Sum of Fibonacci Series

---
+++ Program_36.c
@@ -0,0 +1,21 @@
+// Program to calculate sum of Fibonacci Series.
+
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int i, a, b, c, num, sum = 0;
+    printf("Enter number of terms : ");
+    scanf("%d", &num);
+    a = 0, b = 1;
+    sum = a + b;
+    for (i = 2; i < num; i++)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+        sum = sum + c;
+    }
+    printf("\nSum :%d", sum);
+    getch();
+}
