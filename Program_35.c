0867e5c4123a9400d3a495de3526c409c5f92e5e
KapilSinghMaan <kapilmaan369@gmail.com>
2025-04-01 22:12:21 +0530

    All factors of a number

---
+++ Program_35.c
@@ -0,0 +1,19 @@
+// Program to print all factors of a number.
+
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int num, i;
+    printf("Enter any Number : ");
+    scanf("%d", &num);
+    printf("\nFactors of %d are : 1", num);
+    for (i = 2; i <= num; i++)
+    {
+        if (num % i == 0)
+        {
+            printf(", %d", i);
+        }
+    }
+    getch();
+}
